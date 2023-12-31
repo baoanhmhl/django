9f9d25656ad54b65ca015cf5dd7dd28957447a75
baoanhmhl <baoanhtruong7676@gmail.com>
2023-12-31 18:45:09 +0700

    first commit

---
+++ chiaman.py
@@ -0,0 +1,5 @@
+k=int(input("Số mận"))
+n=int(input("số học sinh"))
+taothua=k//n
+print("Mỗi học sinh được số quả mận là",taothua)
+print("Số táo dành riêng cho các em nữ",k-n*taothua)
---
+++ tamgiacvuong.py
@@ -0,0 +1,9 @@
+import math
+a=int(input("nhập cạnh thứ nhất"))
+b=int(input("nhập cạnh thứ hai"))
+print("Diện tích tam giác",a*b/2)
+print("Cạnh huyền",math.sqrt(a*a+b*b))
+if a>0:
+    print(b)
+
+
